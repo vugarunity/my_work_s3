5c291b682f4ead4678fd17764f519f4fcf9ad417
Your Name <you@example.com>
2024-10-22 17:00:12 +0400

    StudentGroups

---
+++ StudentGroups.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class StudentGroups {
+    private List<StudentGroups> studentGroupsList;
+
+    public List<StudentGroups> getStudentGroupsList() {
+        return studentGroupsList;
+        
+    }
+
+    public void setStudentGroupsList(List<StudentGroups> studentGroupsList) {
+        this.studentGroupsList = studentGroupsList;
+    }
+    
+}
+
+
